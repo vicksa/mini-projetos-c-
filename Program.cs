dd10c8c64047db8695b3beffbed65e7306879068
Victória Arine <victoria.lourencini14@gmail.com>
2026-01-04 14:38:50 -0300

    Meu primeiro projeto: Simulador de Dados

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class SimuladorDados
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("===== Simulador de Dados =====");
+        Console.WriteLine("Pressione Enter para rolar o dado");
+        Console.ReadKey();
+
+        Random gerador = new Random();
+
+        
+        int resultado = gerador.Next(1, 7);
+        
+        Console.WriteLine($"\nO dado foi rolado...");
+        Console.WriteLine($"O resultado é: {resultado}");
+
+        Console.WriteLine("\nPressione Enter para sair");
+        Console.ReadKey();
+    }
+}
